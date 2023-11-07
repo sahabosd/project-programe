4ca71cd47b01820974ab5afb286e3c8a9c700fcd
sahabosd <sayeed.al.shahab.2003@gmail.com>
2023-11-08 00:23:17 +0600

    r deducted

--- splash.h
+++ splash.h
@@ -1,13 +1,10 @@
-void splash()
+void splash(void)
 {
     printf("\t\t\t\t\t\t\t         ||   / |  / /   \n");
  printf("\t\t\t\t\t\t\t          ||  /  | / /         //   _                                     __               _    \n");
  printf("\t\t\t\t\t\t\t          || / /||/ /  //_))  //  //  )) //   ))  // )) ))  //_))  //\\   ||  ||\\//||   || ))   //\\  ||\\ || || // \n");
  printf("\t\t\t\t\t\t\t          ||/ / |  /  //     //  //     //   //  // // //  //     //--\\  ||  ||    ||   ||_))  //--\\ || \\|| ||//   \n");
  printf("\t\t\t\t\t\t\t          |  /  | /  ((_    //  ((_    ((   //  // // //  ((_    //    \\ ||  ||    ||   ||_)) //    \\||   || || \\  \n");
- printf("wait 3 sec to vanish the above text");
- sleep(3);
-
 }
 
 
